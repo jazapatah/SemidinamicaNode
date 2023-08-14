32b86d70ffb34ac1af4f58961cf58bdcaf593e91
Jonatan <jotan.alexander@gmail.com>
2023-08-13 22:58:41 -0500

    start app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-// importar los paquetes como el express
+// importar los paquetes como el express, para crear un webserver en un entorno local
 const express = require('express');
+// el webserver se le asigne al archivo app.js
+const app = express();
+const port = 9595;
+
+// mostrar contenido de la carpeta public
+app.use(express.static('public'));
